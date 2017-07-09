0e458adea462b8751d6506ca0df377c02f47f77d
Ilaissa Romero <inr211@Ilaissas-iMac.local>
2017-07-09 15:49:05 -0400

    Clean up rest

--- src/main/java/com/moManager/manager/ManagerApplication.java
+++ src/main/java/com/moManager/manager/ManagerApplication.java
@@ -2,10 +2,13 @@ package com.moManager.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan({"com.moManager.data"})
 @SpringBootApplication
+@EntityScan(basePackages = {"com.moManager"})
+@EnableJpaRepositories(basePackages = {"com.moManager"})
 public class ManagerApplication {
 
 	public static void main(String[] args) {
